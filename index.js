3b276fdedf55326ff7f3b8dc50963a630a442668
Paige <paigeberliner@gmail.com>
2023-08-09 15:29:16 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = function(){
+   const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+   return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(){
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return drivers.slice(-2)
+ }
+
+ const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers ]
+
+ function createFareMultiplier(){
+    let fareAmount = 5;
+    const fareMultiplier = fareAmount
+    return function(){
+        return fareAmount * fareMultiplier
+    }
+ }
+
+ function fareDoubler(createFareMultiplier){
+    let fareAmount = 10;
+    const fareDoubler = 2;
+    return fareAmount * fareDoubler
+}
+function fareTripler(){
+    let fareAmount = 12;
+    const fareTripler = 3;
+    return fareAmount * fareTripler;
+}
+
+function selectDifferentDrivers(drivers, selectingDrivers){
+    if(selectingDrivers === returnFirstTwoDrivers){
+        return drivers.slice(0,2)
+    }
+    else {
+        return drivers.slice(-2)
+    }
+}
+
+
+    
+
+  
+
